0f059898175b548e049d81b77af0b2e65080901d
Alessandro Paucar <146753377+paucare@users.noreply.github.com>
2024-04-04 18:20:21 +0200

    Initial Commit

---
+++ src/main/java/org/example/Appearence.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import java.util.List;
+
+public class Appearence {
+    public String gender;
+    public String race;
+    public List<String> height;
+    public List<String> weight;
+    public String eyeColor;
+    public String hairColor;
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public List<String> getHeight() {
+        return height;
+    }
+
+    public void setHeight(List<String> height) {
+        this.height = height;
+    }
+
+    public List<String> getWeight() {
+        return weight;
+    }
+
+    public void setWeight(List<String> weight) {
+        this.weight = weight;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+}
